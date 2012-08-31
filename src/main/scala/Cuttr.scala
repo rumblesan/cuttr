32f399f9cb70293a9fc94d0661332cbb009c779d
Guy John <guy@rumblesan.com>
2012-08-31 23:52:57 +0100

    Improving file reading
    Making better use of the option class

--- src/main/scala/Cuttr.scala
+++ src/main/scala/Cuttr.scala
@@ -22,7 +22,7 @@ object Cuttr {
 
   def run(config:Config) {
 
-    val input = try {
+    val inputfile = try {
       Some(ImageIO.read(new File(config.inputfile)))
     } catch {
       case ioe: IOException => {
@@ -30,41 +30,42 @@ object Cuttr {
       }
     }
 
-    val image = input.get
+    inputfile map { image =>
 
-    val timestamp = new Date().getTime()
-    val rand = new Random(timestamp)
-    val randXShift = rand.nextDouble() * 100
-    val randYShift = rand.nextDouble() * 100
+      val timestamp = new Date().getTime()
+      val rand = new Random(timestamp)
+      val randXShift = rand.nextDouble() * 100
+      val randYShift = rand.nextDouble() * 100
 
-    val (width, height) = image.getSize()
+      val (width, height) = image.getSize()
 
-    val shiftXFunc = (pos:Int) => { round(randXShift + pos + (10 * cos(pos))).toInt}
-    val shiftYFunc = (pos:Int) => { round(randYShift + pos + (10 * cos(pos))).toInt}
+      val shiftXFunc = (pos:Int) => { round(randXShift + pos + (10 * cos(pos))).toInt}
+      val shiftYFunc = (pos:Int) => { round(randYShift + pos + (10 * cos(pos))).toInt}
 
-    val shifter = PixelTransform.funcVar(width-1, height-1, shiftXFunc, shiftYFunc)
+      val shifter = PixelTransform.funcVar(width-1, height-1, shiftXFunc, shiftYFunc)
 
-    val output = new BufferedImage(width, height, image.getType())
+      val output = new BufferedImage(width, height, image.getType())
 
-    for (x <- 0 until width) {
-      for (y <- 0 until height) {
+      for (x <- 0 until width) {
+        for (y <- 0 until height) {
 
-        val originalColor = image.getPixel(x, y)
+          val originalColor = image.getPixel(x, y)
 
-        val (redX, redY) = shifter(x, y)
-        val (greenX, greenY) = shifter(redX, redY)
+          val (redX, redY) = shifter(x, y)
+          val (greenX, greenY) = shifter(redX, redY)
 
-        val prevRed = image.getPixel(redX, redY).scale(0.5)
-        val prevGreen = image.getPixel(greenX, greenY).scale(0)
+          val prevRed = image.getPixel(redX, redY).scale(0.5)
+          val prevGreen = image.getPixel(greenX, greenY).scale(0)
 
-        output.setPixel(x, y, new Color(prevRed.getRed(), prevGreen.getGreen(), originalColor.getBlue()))
+          output.setPixel(x, y, new Color(prevRed.getRed(), prevGreen.getGreen(), originalColor.getBlue()))
+        }
       }
+
+      ImageIO.write(output, "jpg", new File("output.jpeg"))
+
     }
 
-    print("Writing")
-    ImageIO.write(output, "jpg", new File("output.jpeg"))
-
   }
-}
 
+}
 
