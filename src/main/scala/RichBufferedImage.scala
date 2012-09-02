d40c0322178f988202a034cee794606780d8ec03
Guy John <guy@rumblesan.com>
2012-09-02 21:26:55 +0100

    Adding addPixel and float get/set functions

--- src/main/scala/RichBufferedImage.scala
+++ src/main/scala/RichBufferedImage.scala
@@ -8,15 +8,51 @@ import RichColor._
 
 case class RichBufferedImage(image:BufferedImage) {
 
-  def getPixel(xVal:Int, yVal:Int) = {
-    new Color (image.getRGB(xVal, yVal))
+  def getPixel(x:Int, y:Int):Color = {
+    new Color (image.getRGB(x, y))
   }
 
-  def setPixel(xVal:Int, yVal:Int, c:Color) = {
-    image.setRGB(xVal, yVal, c.getRGB())
+  def getPixel(x:Double, y:Double):Color = {
+
+    val a = image.getPixel((x.toInt + 0), (y.toInt + 0))
+    val b = image.getPixel((x.toInt + 1), (y.toInt + 0))
+    val c = image.getPixel((x.toInt + 0), (y.toInt + 1))
+    val d = image.getPixel((x.toInt + 1), (y.toInt + 1))
+
+    (a * (1 - (x % 1)) * (1 - (y % 1))) +
+    (b * ((x % 1)) * (1 - (y % 1))) +
+    (c * (1 - (x % 1)) * ((y % 1))) +
+    (d * ((x % 1)) * ((y % 1)))
   }
 
-  def getSize() = {
+  def setPixel(x:Int, y:Int, c:Color):Unit = {
+    image.setRGB(x, y, c.getRGB())
+  }
+
+  def addPixel(x:Int, y:Int, c:Color):Unit = {
+    val currentColor = image.getPixel(x,y)
+    val newColor = currentColor + c
+    image.setRGB(x, y, newColor.getRGB())
+  }
+
+  def addPixel(x:Double, y:Double, c:Color):Unit = {
+    val a = (1 - (x % 1)) * (1 - (y % 1))
+    val b = ((x % 1)) * (1 - (y % 1))
+    val c = (1 - (x % 1)) * ((y % 1))
+    val d = ((x % 1)) * ((y % 1))
+
+    val pA = image.getPixel((x.toInt + 0), (y.toInt + 0))
+    val pB = image.getPixel((x.toInt + 1), (y.toInt + 0))
+    val pC = image.getPixel((x.toInt + 0), (y.toInt + 1))
+    val pD = image.getPixel((x.toInt + 1), (y.toInt + 1))
+
+    image.setPixel((x.toInt + 0), (y.toInt + 0), pA * a)
+    image.setPixel((x.toInt + 1), (y.toInt + 0), pB * b)
+    image.setPixel((x.toInt + 0), (y.toInt + 1), pC * c)
+    image.setPixel((x.toInt + 1), (y.toInt + 1), pD * d)
+  }
+
+  def getSize():Pair[Int, Int] = {
     val width  = image.getWidth()
     val height = image.getHeight()
     (width, height)
