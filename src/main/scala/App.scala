35b94bcd76a034bb18af6d56626601203df6b7c7
Guy John <guy@rumblesan.com>
2012-09-11 01:45:51 +0100

    Throwing it all together
    It's working reasonably well now, getting images and updating tumblr
    Some of the code is still pretty scrappy but I'll neaten that up later.

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -34,7 +34,8 @@ object App {
     // flatMap across the blogs, getting the info from each one
     blogs.flatMap(
       blogUrl => {
-        tumblr.get("info", blogUrl).map(
+        println("Getting info for %s".format(blogUrl))
+        tumblr.get("info", blogUrl, Map("type" -> "info")).map(
           info => {
             info.asInstanceOf[InfoQuery].blog
           }
@@ -45,6 +46,8 @@ object App {
 
   def getBlogPhotos(tumblr:TumblrAPI, blogInfo:BlogInfo, tag:String = ""):List[PhotoInfo] = {
 
+    println("Getting photos for %s".format(blogInfo.title))
+
     val defaultParams = Map("type" -> "photo")
 
     val params = if (tag.isEmpty) {
@@ -53,8 +56,17 @@ object App {
       defaultParams + (("tag", tag))
     }
 
+
+    // The API doesn't handle the leading http:// or trailing slash
+    // It bloody well should, fix it you git
+    val urlRegex = """http://(.*)/""".r
+    val urlRegex(cleanUrl) = blogInfo.url
+
     // Option[PhotoQuery]
-    val blogPosts = tumblr.get("posts", blogInfo.url, defaultParams).map(_.asInstanceOf[PhotoQuery])
+    val blogPosts = tumblr.get("posts", cleanUrl, params).map(_.asInstanceOf[PhotoQuery])
+
+    println(blogInfo.url)
+    println(blogPosts)
 
     blogPosts.map(
       query => {
@@ -75,11 +87,12 @@ object App {
   }
 
   def createPostCaption(info:PhotoInfo) = {
-    "<a href='%s'>Original</a>" +
-    " image courtesy of " +
-    "<a href='%s'>%s</a>" +
-    "\n" +
-    "<a href='%s'> First posted</a> on %s".format(
+    val description = "<a href='%s'>Original</a>" +
+                      " image courtesy of " +
+                      "<a href='%s'>%s</a>" +
+                      "\n" +
+                      "<a href='%s'> First posted</a> on %s"
+    description.format(
       info.imgUrl,
       info.blogUrl,
       info.blogName,
@@ -110,42 +123,48 @@ object App {
                                   oauthCfg("oauthToken"),
                                   oauthCfg("oauthSecret"))
 
+    println("Connected to Tumblr")
+
     val tag = cfg("search")("tag")
     val allBlogInfo = getAllBlogInfo(tumblrApi, blogList)
+    println(allBlogInfo)
     val allPhotos   = allBlogInfo.flatMap(getBlogPhotos(tumblrApi, _, tag))
 
+    println("Got %d photos".format(allPhotos.length))
     val selection = Random.shuffle(allPhotos).headOption
+    println(selection)
 
-    val imageFile = selection.map(
+    selection.map(
       imageInfo => {
-        ImageIO.read(new URL(imageInfo.imgUrl))
+        val image = ImageIO.read(new URL(imageInfo.imgUrl))
+
+        println("Getting it")
+
+        val glitcher = new Cuttr(image)
+        val glitchedImage = glitcher.glitch()
+
+        val baos = new ByteArrayOutputStream()
+        ImageIO.write(glitchedImage, "jpg", baos)
+        baos.flush()
+        val imageData = baos.toByteArray()
+        baos.close()
+
+        val postCaption = createPostCaption(imageInfo)
+        println(postCaption)
+
+        val params = Map("type" -> "photo",
+                         "caption" -> postCaption,
+                         "tags" -> "Cuttr, glitch, generative, random")
+
+        val imgResponse = tumblrApi.post("post",
+                                         blogUrl,
+                                         params,
+                                         imageData)
+        println(imgResponse)
+
+        ImageIO.write(glitchedImage, "jpg", new File("output.jpeg"))
       }
     )
-
-    imageFile map { image =>
-
-      val glitcher = new Cuttr(image)
-      val glitchedImage = glitcher.glitch()
-
-      val baos = new ByteArrayOutputStream()
-      ImageIO.write(glitchedImage, "jpg", baos)
-      baos.flush()
-      val imageData = baos.toByteArray()
-      baos.close()
-
-      val params = Map("type" -> "photo",
-                       "caption" -> "Haha, looks like this is all coming together!",
-                       "tags" -> "testing, glitch, generative, random")
-
-      val imgResponse = tumblrApi.post("post",
-                                       blogUrl,
-                                       params,
-                                       imageData)
-      println(imgResponse)
-
-     ImageIO.write(glitchedImage, "jpg", new File("output.jpeg"))
-
-    }
 
   }
 
