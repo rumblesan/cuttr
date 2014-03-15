48a19aa3d7e4a64bf2bfe86a23515a0b5dfda370
Guy John <guy@rumblesan.com>
2014-03-15 12:57:25 +0000

    Moving new pusher code into its own object

--- scala-glitch/src/main/scala/Cuttr.scala
+++ scala-glitch/src/main/scala/Cuttr.scala
@@ -28,54 +28,7 @@ class Cuttr(image:BufferedImage) {
 
   def smear(): BufferedImage = Smear(image)
 
-  def pushed(): List[BufferedImage] = {
-
-    val rand = new Random()
-
-    val frames = rand.nextInt(100) + 10
-
-    val shiftGenerator: Int => Int => (Int => Int) = shift => multiplier => {
-      (pos: Int) => {
-        (shift + pos + (multiplier * cos(pos))).toInt
-      }
-    }
-
-    val glitcher: BufferedImage => (Int => Int) => (Int => Int) => BufferedImage = image => shiftXFunc => shiftYFunc => {
-
-      val (width, height) = image.getSize()
-      val shifter = wrapCoords(width-1, height-1, shiftXFunc, shiftYFunc)
-
-      for (x <- 0 until width) {
-        for (y <- 0 until height) {
-
-          val coords = (x, y)
-
-          val rShiftCoords = shifter(coords)
-          val gShiftCoords = shifter(rShiftCoords)
-          val bShiftCoords = shifter(gShiftCoords)
-
-          val newRed   = image.getPixel(rShiftCoords)
-          val newGreen = image.getPixel(gShiftCoords)
-          val newBlue  = image.getPixel(bShiftCoords)
-
-          image.setRed(coords, newRed.getRed())
-          image.setGreen(coords, newGreen.getGreen())
-          image.setBlue(coords, newBlue.getBlue())
-
-        }
-      }
-
-    image
-
-    }
-
-    for {
-      f <- (1 to frames)
-    } yield f
-
-    List(image)
-
-  }
+  def pusher(): List[BufferedImage] = Pusher(image)
 
   def cubist(): BufferedImage = Cubist(image)
 
---
+++ scala-glitch/src/main/scala/glitches/Pusher.scala
@@ -0,0 +1,65 @@
+package com.rumblesan.scalaglitch.glitches
+
+import scala.util.Random
+import math.cos
+
+import com.rumblesan.scalaglitch.RichBufferedImage._
+import com.rumblesan.scalaglitch.CoordWrapper.wrapCoords
+import com.rumblesan.scalaglitch.RichColor._
+
+import java.awt.image.BufferedImage
+
+
+object Pusher {
+
+  def apply(image: BufferedImage): List[BufferedImage] = {
+ 
+    val rand = new Random()
+
+    val frames = rand.nextInt(100) + 10
+
+    val shiftGenerator: Int => Int => (Int => Int) = shift => multiplier => {
+      (pos: Int) => {
+        (shift + pos + (multiplier * cos(pos))).toInt
+      }
+    }
+
+    val glitcher: BufferedImage => (Int => Int) => (Int => Int) => BufferedImage = image => shiftXFunc => shiftYFunc => {
+
+      val (width, height) = image.getSize()
+      val shifter = wrapCoords(width-1, height-1, shiftXFunc, shiftYFunc)
+
+      for (x <- 0 until width) {
+        for (y <- 0 until height) {
+
+          val coords = (x, y)
+
+          val rShiftCoords = shifter(coords)
+          val gShiftCoords = shifter(rShiftCoords)
+          val bShiftCoords = shifter(gShiftCoords)
+
+          val newRed   = image.getPixel(rShiftCoords)
+          val newGreen = image.getPixel(gShiftCoords)
+          val newBlue  = image.getPixel(bShiftCoords)
+
+          image.setRed(coords, newRed.getRed())
+          image.setGreen(coords, newGreen.getGreen())
+          image.setBlue(coords, newBlue.getBlue())
+
+        }
+      }
+
+    image
+
+    }
+
+    for {
+      f <- (1 to frames)
+    } yield f
+
+    List(image)
+
+  }
+
+}
+
