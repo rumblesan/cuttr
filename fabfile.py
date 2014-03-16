d15795d9df2f3403b98fe39898361a4fc378e2ec
Guy John <guy@rumblesan.com>
2014-03-16 14:31:17 +0000

    Updating fabric deployment script

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 from __future__ import with_statement
-from fabric.api import local, put, run
+from fabric.api import local, put, run, env
+
+env.use_ssh_config = True
 
 
 def build():
     local('sbt clean compile assembly')
 
 
-def deploy(user, group):
+def deploy():
     put('assembled/cuttr.jar', '~/tumblrscripts/cuttr')
-    run('chmod +x /home/%s/tumblrscripts/cuttr/cuttr.jar' % user)
+    run('chmod +x /home/%s/tumblrscripts/cuttr/cuttr.jar' % env.GLITCHUSER)
     run('chown %s:%s /home/%s/tumblrscripts/cuttr/cuttr.jar' %
-        (user, group, user))
+        (env.GLITCHUSER, env.GLITCHGROUP, env.GLITCHUSER))
