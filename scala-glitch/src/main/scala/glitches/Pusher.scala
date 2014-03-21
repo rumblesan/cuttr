bfb9dfc5261e792f8fafde34ac568a2dfd358665
Guy John <guy@rumblesan.com>
2014-03-21 22:13:08 +0000

    Filling out Pusher functionality

--- scala-glitch/src/main/scala/glitches/Pusher.scala
+++ scala-glitch/src/main/scala/glitches/Pusher.scala
@@ -1,7 +1,11 @@
 package com.rumblesan.scalaglitch.glitches
 
+import com.rumblesan.scalaglitch.types._
+
+import scalaz._, Scalaz._
+
 import scala.util.Random
-import math.cos
+import math.{cos, sin, Pi}
 
 import com.rumblesan.scalaglitch.util.RichBufferedImage._
 import com.rumblesan.scalaglitch.util.CoordWrapper.wrapCoords
@@ -10,54 +14,82 @@ import com.rumblesan.scalaglitch.util.RichColor._
 import java.awt.image.BufferedImage
 
 
-object Pusher {
+object Pusher extends GlitchTypes {
 
   def apply(image: BufferedImage): List[BufferedImage] = {
  
     val rand = new Random()
 
-    val frames = rand.nextInt(100) + 10
+    val (width, height) = image.getSize()
 
-    val shiftGenerator: Int => Int => (Int => Int) = shift => multiplier => {
-      (pos: Int) => {
-        (shift + pos + (multiplier * cos(pos))).toInt
+    val genGlitches = for {
+      frames <- getFrameCount
+      multiplier <- getFrameOffsetMultiplier
+      frameOffset = (Pi / frames).toInt
+      shifterGen <- shifterGenerator(width)(height)
+      images = for {
+        f <- (1 to frames)
+        offset = sin(f * frameOffset) * multiplier
+        shifter = shifterGen(offset)
+        runPusher = pusherGenerator(shifter)
+      } yield image.createGlitch(runPusher)
+    } yield images
+
+    genGlitches.eval(rand).toList
+
+  }
+
+  val getFrameCount = State[Random, Int] { rand =>
+    (rand, rand.nextInt(300) + 100)
+  }
+
+  val getFrameOffsetMultiplier = State[Random, Int] { rand =>
+    (rand, rand.nextInt(100))
+  }
+
+  val createShift: Int => State[Random, Int] = shiftMultiplier => State[Random, Int] { rand =>
+    (rand, (rand.nextDouble() * shiftMultiplier).toInt)
+  }
+
+
+  val shifterGenerator: Int => Int => State[Random, Double => PixelShifter] = width => height => for {
+    randXShift <- createShift(100)
+    randYShift <- createShift(100)
+    xShifter = (mult: Double) => createShifter(randXShift)(mult)
+    yShifter = (mult: Double) => createShifter(randYShift)(mult)
+  } yield (mult: Double) => wrapCoords(width - 1, height - 1, xShifter(mult), yShifter(mult))
+
+  val createShifter: Int => Double => (Int => Int) = randShift => multiplier => {
+    (pos) => {
+      (randShift + pos + (multiplier * cos(pos))).toInt
+    }
+  }
+
+  val pusherGenerator: PixelShifter => BufferedImage => BufferedImage = shifter => image => {
+
+    val (width, height) = image.getSize()
+
+    for (x <- 0 until width) {
+      for (y <- 0 until height) {
+
+        val coords = (x, y)
+
+        val rShiftCoords = shifter(coords)
+        val gShiftCoords = shifter(rShiftCoords)
+        val bShiftCoords = shifter(gShiftCoords)
+
+        val newRed   = image.getPixel(rShiftCoords)
+        val newGreen = image.getPixel(gShiftCoords)
+        val newBlue  = image.getPixel(bShiftCoords)
+
+        image.setRed(coords, newRed.getRed())
+        image.setGreen(coords, newGreen.getGreen())
+        image.setBlue(coords, newBlue.getBlue())
+
       }
     }
-
-    val glitcher: BufferedImage => (Int => Int) => (Int => Int) => BufferedImage = image => shiftXFunc => shiftYFunc => {
-
-      val (width, height) = image.getSize()
-      val shifter = wrapCoords(width-1, height-1, shiftXFunc, shiftYFunc)
-
-      for (x <- 0 until width) {
-        for (y <- 0 until height) {
-
-          val coords = (x, y)
-
-          val rShiftCoords = shifter(coords)
-          val gShiftCoords = shifter(rShiftCoords)
-          val bShiftCoords = shifter(gShiftCoords)
-
-          val newRed   = image.getPixel(rShiftCoords)
-          val newGreen = image.getPixel(gShiftCoords)
-          val newBlue  = image.getPixel(bShiftCoords)
-
-          image.setRed(coords, newRed.getRed())
-          image.setGreen(coords, newGreen.getGreen())
-          image.setBlue(coords, newBlue.getBlue())
-
-        }
-      }
 
     image
-
-    }
-
-    for {
-      f <- (1 to frames)
-    } yield f
-
-    List(image)
 
   }
 
