e0ddc7115248ea2062820fdc6cf86a1456b72e9c
Guy John <guy@rumblesan.com>
2015-04-26 17:11:48 +0100

    Moving JpegGlitcher into its own file

--- scala-glitch/src/main/scala/glitches/Errror.scala
+++ scala-glitch/src/main/scala/glitches/Errror.scala
@@ -15,26 +15,6 @@ import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 
 
-object JpegGlitcher {
-
-  def glitchQuantTable(struct: JpegStructureData, glitchChance: Int): JpegStructureData = {
-
-    val r = new Random()
-    val glitched = struct.data.drop(3).map(b => {
-      if (r.nextInt(100) < glitchChance) {
-        (r.nextInt(253) + 1).toByte
-      } else {
-        b
-      }
-    })
-
-    struct.copy(data = (struct.data.take(3) ++ glitched))
-
-  }
-
-}
-
-
 object Errror extends GlitchTypes {
 
   def apply(imageFile: File): GlitchedImage = {
